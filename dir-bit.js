faa177bb4ed2b38317e948583dd4f8ab5e2d2b93
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2021-10-21 17:16:05 +0000

    Update dir-bit.js

--- dir-bit.js
+++ dir-bit.js
@@ -70,5 +70,5 @@ function _delFss(path,fileArr,start,callback,arr){delFiless(path,fileArr,start,c
   * @param {object} new_fs `fs` module overriding object.
   */
  function o(new_fs){fs=new_fs}
- module.exports = {delFiles:_delf,rmdir:removeDir,overidefs:o};
+ module.exports = {deleteFiles:_delf,removeDir:removeDir,overidefs:o};
 
